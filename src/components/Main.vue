e3b3083e1152e95e4c50642b5c8bb5d1f67b9b69
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2018-10-04 23:28:35 +0900

    Split about me section.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,34 +2,7 @@
   <div id="main">
     <main>
       <article>
-        <section id="section-about-me">
-          <h2 class="section-titles">T. Matsudateについて</h2>
-          <div class="section-contents">
-            <ul id="about-me">
-              <li>氏名: Tsuyoshi Matsudate</li>
-              <li>最終職歴: 株式会社インフィニットループ(Web Developer(at 2015))
-                <ul id="last-environment">
-                  <li><strong class="captions">開発環境</strong>
-                    <ul class="items">
-                      <li>PHP(maybe 5.x)</li>
-                      <li>CakePHP (&lt;2.6)</li>
-                      <li>Bootstrap</li>
-                      <li>jQuery</li>
-                      <li>Windows 8 + Vagrant</li>
-                    </ul>
-                  </li>
-                  <li><strong class="captions">担当した製品</strong>
-                    <ul id="last-product">
-                      <li>製品名: Kamimage(カミメージ)</li>
-                      <li>概要: アンケート等の入力フォームを紙媒体の感触を可能な限り再現した上で提供します。</li>
-                      <li>URL: <a href="https://www.kamimage.com">www.kamimage.com</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </section>
+        <AboutMe />
         <section id="section-resume">
           <h2 class="section-titles">職務経歴(日時降順)</h2>
           <div class="section-contents">
@@ -150,8 +123,13 @@
 </template>
 
 <script>
+import AboutMe from './main/AboutMe.vue'
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    components: {
+        AboutMe
+    }
 }
 </script>
 
@@ -178,7 +156,7 @@ export default {
             }
 
             #section-about-me {
-                ul#about-me {
+                #about-me {
                     list-style: none;
                     margin: 1em;
 
---
+++ src/components/main/AboutMe.vue
@@ -0,0 +1,83 @@
+<template>
+  <section id="section-about-me">
+    <h2 class="section-titles">T. Matsudateについて</h2>
+    <div class="section-contents">
+      <ul id="about-me">
+        <li>氏名: Tsuyoshi Matsudate</li>
+        <li>最終職歴: 株式会社インフィニットループ(Web Developer(at 2015))
+          <ul id="last-environment">
+            <li><strong class="captions">開発環境</strong>
+              <ul class="items">
+                <li>PHP(maybe 5.x)</li>
+                <li>CakePHP (&lt;2.6)</li>
+                <li>Bootstrap</li>
+                <li>jQuery</li>
+                <li>Windows 8 + Vagrant</li>
+              </ul>
+            </li>
+            <li><strong class="captions">担当した製品</strong>
+              <ul id="last-product">
+                <li>製品名: Kamimage(カミメージ)</li>
+                <li>概要: アンケート等の入力フォームを紙媒体の感触を可能な限り再現した上で提供します。</li>
+                <li>URL: <a href="https://www.kamimage.com">www.kamimage.com</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'AboutMe'
+}
+</script>
+
+<style lang="less">
+#grids {
+    #main {
+        main {
+            section {
+                &-titles {
+                    font-size: 150%;
+                    font-weight: bold;
+                    font-style: normal;
+                    padding: 0em;
+                }
+
+                &-contents {
+                    padding: 1em;
+                }
+            }
+
+            #section-about-me {
+                #about-me {
+                    list-style: none;
+                    margin: 1em;
+                }
+
+                #last-environment {
+                    list-style: none;
+                    margin: 1em;
+                }
+
+                #last-product {
+                    list-style: outside;
+                    margin: 1em;
+                }
+
+                .captions {
+                    font-weight: bold;
+                }
+
+                .items {
+                    list-style: outside;
+                    margin: 1em;
+                }
+            }
+        }
+    }
+}
+</style>
